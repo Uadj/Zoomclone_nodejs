059dd27325ad6fd72021e8bfd71d705e4124083a
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-28 17:15:18 +0900

    #1 chat program complted

--- src/server.js
+++ src/server.js
@@ -25,12 +25,19 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        message = message.toString("utf-8")
-      sockets.forEach((aSocket) => aSocket.send(message));
+    socket.on("message", (msg) => {
+      const message = JSON.parse(msg);
+      switch (message.type) {
+        case "new_message":
+          sockets.forEach((aSocket) =>
+            aSocket.send(`${socket.nickname}: ${message.payload}`)
+          );
+        case "nickname":
+          socket["nickname"] = message.payload;
+      }
     });
   });
-  
   server.listen(3000, handleListen);
